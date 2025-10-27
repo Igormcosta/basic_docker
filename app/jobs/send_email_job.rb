1963d7ede3d69d9e6740ac048a112e85e63dce9f
Igor Medeiros <igormedeiros1999@gmail.com>
2025-10-27 08:43:51 -0300

    add rubocop correction

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -2,10 +2,8 @@ class SendEmailJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    
     puts "====== Performing SendEmailJob..."
-    sleep 10 
+    sleep 10
     puts "====== Email sent with args: #{args.inspect}"
-    
   end
 end
